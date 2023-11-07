8cbfbcefd7e71a64fd88e1d236f52d0eb87f8ba6
yelsiño <yelsino321@gmai.com>
2023-11-07 16:32:54 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                     <Route
                         path="/"
                         element={
-                            <div className="max-w-6xl mx-auto relative  flex flex-col  ">
+                            <div className="max-w-5xl mx-auto relative  flex flex-col  ">
                                 <Header />
                                 <div className="px-10 sm:px-20 flex flex-col gap-y-10 ">
                                     <Home />
--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -12,16 +12,17 @@ const Portflolio = () => {
         <p className="text-color_purple3">mis proyectos</p>
         <h2 className="text-3xl">Portafolio</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-1  lg:grid-cols-2 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-1  lg:grid-cols-1 gap-10">
         {proyects.map((p, index) => (
           <div key={index} className=" bg-color_purple2 p-5 rounded-3xl ">
-            <div className="max-w-xl mx-auto flex flex-col gap-y-7 ">
-              <img className="rounded-2xl object-cover" src={p.img} />
+            <div className=" mx-auto flex flex-col gap-y-7 ">
+              {/* <div className="flex justify-center  max-w-2xl object-cover"> */}
+                <img className="rounded-2xl object-cover " src={p.img} />
+              {/* </div> */}
+
               <div className="text-xl text-left font-bold font-poppins ">
                 {p.description}
               </div>
-
-        
 
               <div className="text-lg text-left font-dm-sans text-color_purple3">
                 {p.detalle}
@@ -29,11 +30,14 @@ const Portflolio = () => {
               <div className="flex gap-x-5 flex-col">
                 <p className="text-left pb-2">Desarrollado con</p>
                 <div className="flex flex-wrap gap-x-5 gap-y-2 ">
-                {p.tools.map(({text},index) => (
-                  <span key={index}
-                    className=' font-semibold text-color_purple3'
-                  >{text}</span>
-                ))}
+                  {p.tools.map(({ text }, index) => (
+                    <span
+                      key={index}
+                      className=" font-semibold text-color_purple3"
+                    >
+                      {text}
+                    </span>
+                  ))}
                 </div>
               </div>
               <div className=" flex justify-start gap-x-10">
@@ -60,7 +64,6 @@ const Portflolio = () => {
             </div>
           </div>
         ))}
-
       </div>
     </section>
   );
@@ -70,8 +73,21 @@ const Portflolio = () => {
 
 export default Portflolio;
 
-
-const {javascript,tailwindcss,nodejs,mongodb,figma,reactjs,socketio, typescript, springboot,antdesign, next, openai, chatgpt } = alltools
+const {
+  javascript,
+  tailwindcss,
+  nodejs,
+  mongodb,
+  figma,
+  reactjs,
+  socketio,
+  typescript,
+  springboot,
+  antdesign,
+  next,
+  openai,
+  chatgpt,
+} = alltools;
 
 const proyects = [
   {
@@ -82,7 +98,7 @@ const proyects = [
       "Esta aplicación web  a las personas seleccionar sus productos en distintas listas y hacer un pedidos de aquellas, esto brinda al cliente flexibilidad y ahorro de tiempo al comprar productos habituales como las papas que siempre se utiliza en una cocina o productos de una receta para días especiales.",
     repository: "https://github.com/yelsino/ns-carlos-tienda-v2",
     liveDemo: "https://yola-satipo.web.app/",
-    tools: [javascript,tailwindcss,nodejs,mongodb,figma,reactjs,socketio],
+    tools: [javascript, tailwindcss, nodejs, mongodb, figma, reactjs, socketio],
   },
   {
     img: screen2,
@@ -92,36 +108,50 @@ const proyects = [
       "Esta aplicación está construida para el control y gestión de listas de pedidos que se generan a través de una tienda online, además controla los créditos de clientes presenciales del negocio.",
     repository: "https://github.com/yelsino/ncarlos-admin-v2",
     liveDemo: "https://admin-ncarlos.web.app/ventas/pedidos",
-    tools: [javascript,tailwindcss,nodejs,mongodb,figma,reactjs,socketio],
+    tools: [javascript, tailwindcss, nodejs, mongodb, figma, reactjs, socketio],
   },
- 
+
   {
     img: screen3,
     description:
       "Landing page una web para atraer clientes con necesidades de servicios informáticos.",
-    detalle: <p> Esta landing page fue construida para
-      <a href="https://idexaperu.com/" target='_blank' className="text-color_blue1" > Idexa Perú</a> una reciente empresa en el mercado para solucionar distintos problemas informáticos en la localidad de Huancayo principalmente.
-    </p>,
+    detalle: (
+      <p>
+        {" "}
+        Esta landing page fue construida para
+        <a
+          href="https://idexaperu.com/"
+          target="_blank"
+          className="text-color_blue1"
+        >
+          {" "}
+          Idexa Perú
+        </a>{" "}
+        una reciente empresa en el mercado para solucionar distintos problemas
+        informáticos en la localidad de Huancayo principalmente.
+      </p>
+    ),
     repository: "https://github.com/yelsino/portafolio",
     liveDemo: "https://idexaperu.com/",
-    tools: [javascript,tailwindcss,figma,reactjs],
+    tools: [javascript, tailwindcss, figma, reactjs],
   },
   {
-    img: 'https://res.cloudinary.com/dwkfj5sxb/image/upload/v1699313857/SCREEN-MY-APPS/voice-app.png',
-    description: "Aplicación web para generar listas con voz e inteligencia artificial.",
+    img: "https://res.cloudinary.com/dwkfj5sxb/image/upload/v1699313857/SCREEN-MY-APPS/voice-app.png",
+    description:
+      "Aplicación web para generar listas con voz e inteligencia artificial.",
     detalle:
       "Esta aplicación web diseñada para dispositivos móviles donde puedes crear listas con voz y gernerar vaucher de pago.",
     repository: "https://github.com/yelsino/list-with-voice",
     liveDemo: "https://list-with-voice.vercel.app",
-    tools: [typescript,tailwindcss,reactjs, next, openai,chatgpt],
+    tools: [typescript, tailwindcss, reactjs, next, openai, chatgpt],
   },
   {
-    img: 'https://res.cloudinary.com/dwkfj5sxb/image/upload/v1678900232/SCREEN-MY-APPS/Sistema_de_prestamos_epfksy.png',
+    img: "https://res.cloudinary.com/dwkfj5sxb/image/upload/v1678900232/SCREEN-MY-APPS/Sistema_de_prestamos_epfksy.png",
     description: "Aplicación para gestionar prestamos de dinero a clientes.",
     detalle:
       "Facilita la gestión de préstamos y el seguimiento de las operaciones de préstamo entre prestamistas y prestatarios. Cuenta con seguimiento de pagos y vencimientos de préstamos y un modulo de generación de informes financieros.",
     repository: "",
     liveDemo: "https://sistema-pretamista.web.app/",
-    tools: [typescript,tailwindcss, reactjs, antdesign, nodejs],
+    tools: [typescript, tailwindcss, reactjs, antdesign, nodejs],
   },
 ];
